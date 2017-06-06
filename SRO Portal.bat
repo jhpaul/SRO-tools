f57b365bb25edb7e8b7f9e13411cbf0142f29a4e
jhpaul <jacobhpaul@gmail.com>
2017-06-06 11:28:25 -0400

    new SRO url

--- SRO Portal.bat
+++ SRO Portal.bat
@@ -1,3 +1,3 @@
 powershell -NoProfile -ExecutionPolicy -Command "& {Stop-Process -processname iexplore}"
 
-"C:\Program Files\Internet Explorer\iexplore.exe" http://portal.riphil.org
+"C:\Program Files\Internet Explorer\iexplore.exe" https://riphil.sro4.com
